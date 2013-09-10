fea7a5710c90e184ce6ed6e2da719f6564890194
Markus Fenske <iblue@gmx.net>
2013-09-10 19:10:08 +0200

    Trial and error...

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source "https://rubygems.org/"
 
 gem "rfuse" # Ruby fuse bindings for the FUSE stuff.
 gem "httparty" # Teh stuffz for teh HTTPs and stuff.
+gem "nokogiri" # Parse HTML
+gem 'byebug' # Debugger
--- lib/bitcasa/session.rb
+++ lib/bitcasa/session.rb
@@ -1,4 +1,38 @@
 module Bitcasa
   class Session
+    include HTTParty
+    base_uri "https://my.bitcasa.com/"
+
+    def initialize(username, password)
+      # Get CSRF token and code parameter
+      response = get "/login"
+      if response.code != 200
+        raise "Server response not ok"
+      end
+      html = Nokogiri::HTML(response)
+      csrf_token = html.css("form input[name=csrf_token]")[0]["value"]
+      code       = html.css("form input[name=code]")[0]["value"]
+
+      # Login using these values
+      response = post "/login", :user       => username,
+                                :password   => password,
+                                :csrf_token => csrf_token,
+                                :code       => code,
+                                :redirect   => "/" # Needed?
+      if response.code == 500
+        raise "Bwah fuck"
+      end
+      byebug
+    end
+
+    private
+    # Convienience methods
+    def post(*args)
+      self.class.post *args
+    end
+
+    def get(*args)
+      self.class.get *args
+    end
   end
 end
