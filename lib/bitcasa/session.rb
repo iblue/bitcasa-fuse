c4763fb64f49a8554090d0e0b398cc1f49541a98
Markus Fenske <iblue@gmx.net>
2013-09-10 19:29:33 +0200

    Add login and get root path

--- lib/bitcasa/session.rb
+++ lib/bitcasa/session.rb
@@ -4,7 +4,7 @@ module Bitcasa
     base_uri "https://my.bitcasa.com/"
 
     def initialize(username, password)
-      # Get CSRF token and code parameter
+      # Get CSRF token, code parameter and session cookie
       response = get "/login"
       if response.code != 200
         raise "Server response not ok"
@@ -13,16 +13,33 @@ module Bitcasa
       csrf_token = html.css("form input[name=csrf_token]")[0]["value"]
       code       = html.css("form input[name=code]")[0]["value"]
 
+      # Persist cookies (Yeah, this is ugly).
+      cookie = response.headers['Set-Cookie']
+
       # Login using these values
-      response = post "/login", :user       => username,
-                                :password   => password,
-                                :csrf_token => csrf_token,
-                                :code       => code,
-                                :redirect   => "/" # Needed?
-      if response.code == 500
-        raise "Bwah fuck"
+      response = post "/login", :body => {
+                                  :user       => username,
+                                  :password   => password,
+                                  :csrf_token => csrf_token,
+                                  :code       => code,
+                                  :redirect   => "/"},
+                                :headers => {
+                                  "Cookie" => cookie
+                                }
+      if response.code != 200
+        raise "Could not login"
       end
+
+      @session_cookie = cookie
+      self.class.headers["Cookie"] = cookie
+
+      # Get root node id
+      @root = (get "/directory/").parsed_response[0]["path"]
       byebug
+    end
+
+    def logout!
+      # Implement me
     end
 
     private
